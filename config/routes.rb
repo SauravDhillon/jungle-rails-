a77708f1fa67bf6610424eb37eb7927cae6e8c0a
Saurav Dhillon <souravdhillon010@gmail.com>
2025-01-27 19:58:41 -0500

    Implemented admin categories controller with actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'categories/index'
+    get 'categories/new'
+    get 'categories/create'
+  end
  #/about route is mapped to about controller index action
   get '/about', to: 'about#index', as: 'about'
 
@@ -17,6 +22,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, only: [:index, :new, :create]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
